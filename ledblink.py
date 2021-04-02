5b3f04514b6a54a12b1d10cc30554b9c9a057904
Matheus Brandão <matheushfbrandao@gmail.com>
2021-04-02 10:22:18 -0300

    ADC change

--- ledblink.py
+++ ledblink.py
@@ -1,10 +1,14 @@
 import machine
 import utime
 led = machine.Pin(25, machine.Pin.OUT)
+pot = machine.ADC(26)
+
+
 while True:
     led(1)
     utime.sleep(1)
     led(0)
     utime.sleep(1)
+    print(pot.read_u16())
     
  
